5196eb5d1e7d6e166453d48bd410e9020f86449a
hfk <felix.kruse1@gmx.de>
2020-02-17 15:22:33 +0100

    end of project

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 Recently I stumbled across the Barnes-Hut algorithm which simplifies solving the n-body problem, i.e. predicting the individual motions of a group of objects interacting gravitationally.
 Since I always wanted to write a program incorporating tree data structures, I decided to implement it.
 
+![n-body simulation](./media/example(n50).mov)
+
 This repository consists of a single file that enables you to compute the individual motions of interacting bodies in 3D.
 
-Barnes Hut algorithms reduce the computational intensity of the N-body problem from U($n^2$) to U(n*log(n)). They do so by partitioning space into subspaces and, given sufficient distance, using the center of mass and mass of subspaces instead of each of the particles contained in them.
+![Octree visual representation](./media/Octree.png)
+<sub>*By WhiteTimberwolf, PNG version: Nü - Own work, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=9851485*</sub>
+
+Barnes Hut algorithms reduce the computational intensity of the N-body problem from U(n<sup>2</sup>) to U(n*log(n)). They do so by partitioning space into subspaces and, given sufficient distance, using the center of mass and mass of subspaces instead of each of the particles contained in them.
 
 You can find more information on how Barnes Hut algorithms work and the N-body problem under the following links:
 
@@ -13,4 +18,4 @@ You can find more information on how Barnes Hut algorithms work and the N-body p
 - [Barnes Hut (interactive)] (https://jheer.github.io/barnes-hut/)
 - [N-body problem] (https://en.wikipedia.org/wiki/N-body_problem) 
 
-*Disclaimer: Normally algorithms like this would be used in astrophysics simulations or particle simulations. Since I did not optimize my code for efficiency, I do not recommend using for any sort of scientific work. If you want to use a barnes-hut algorithm for research you should find a GPU implementation, because those tend to be significantly faster* 
+*Disclaimer: Normally algorithms like this would be used in astrophysics simulations or particle simulations. Since I did not optimize my code for efficiency, I do not recommend using for any sort of scientific work. If you want to use a barnes-hut algorithm for research you should find a GPU implementation, because those tend to be significantly faster.* 
--- barnes_hut.py
+++ barnes_hut.py
@@ -4,7 +4,9 @@ import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
 global grav_const
-grav_const = 0.5
+# The real gravitational constant is about 6.67408e-11; 2 was chosen because the animation will be prettier
+grav_const = 2
+
 
 class OctNode:
     """Octtree node element, with id, center, size, mass, CenterOfMass, gravity, children"""
@@ -14,19 +16,20 @@ class OctNode:
         self.size = size  # maximum side length of the box
         self.children = []  # will later be changed if node has children
 
-        n_points = len(points)
+        n_points = len(points)  # number of points/particles in a box
 
         # only one point in the box
         if n_points == 1:
             leaves.append(self)
-            self.COM = points[0] # set center of mass as point
+            self.COM = points[0]  # set center of mass as point
             self.mass = masses[0]
             self.id = ids[0]
-            self.g = np.zeros(3)  # the gravitational field is what we will insert later
-        # two or more points
+            self.g = np.zeros(3)  # the gravitational field will be inserted later
+
+        # if two or more points subdivide box --> next level of octtree
         else:
             # generate children on the next level of the branch
-            self.SpawnChildren(points, masses, ids, leaves)
+            self.create_children(points, masses, ids, leaves)
             # initialize sum and center of mass variables
             com_total = np.zeros(3)
             m_total = 0.
@@ -38,26 +41,24 @@ class OctNode:
             self.mass = m_total
             self.COM = com_total / self.mass
 
-    def SpawnChildren(self, points, masses, ids, leaves):
+    def create_children(self, points, masses, ids, leaves):
         octant_index = (points > self.center)  # for determining octants
         for i in range(2):  # looping over the 8 octants
             for j in range(2):
                 for k in range(2):
                     # assign points to octants
                     in_octant = np.all(octant_index == np.bool_([i, j, k]), axis=1)
-                    # ToDo simplify? if not in_octant:
-                    if not np.any(in_octant):
+                    if not any(in_octant):
                         continue  # if no points in an octant no node
 
                     dx = 0.5 * self.size * (np.array([i, j, k]) - 0.5)  # calculate center of new child octant
-                    # ToDo how does masses[in_octant], points[in_octant]... work
                     # print(in_octant)
                     self.children.append(OctNode(self.center + dx,
                                                  self.size / 2,
                                                  masses[in_octant],
                                                  points[in_octant],
                                                  ids[in_octant],
-                                                 leaves))
+                                                 leaves))  # store children info in node
 
     def __repr__(self):
         return "OctNodeObject"
@@ -67,7 +68,7 @@ class OctNode:
             f"CenterOfMass({self.COM}), gravity({self.g}, children({self.children})"
 
 
-def TreeWalk(node, node0, thetamax=0.7, G=grav_const):
+def tree_walk(node, node0, thetamax=0.7, gc=grav_const):
     """
     Adds the contribution to the field at node0's point due to particles in node.
     Calling this with the topnode as node will walk the tree to calculate the total field at node0.
@@ -78,90 +79,74 @@ def TreeWalk(node, node0, thetamax=0.7, G=grav_const):
     if r > 0:
         # if single node or theta larger than distance add the gravitational field contribution to node.g
         if (len(node.children) == 0) or (node.size / r < thetamax):
-            node0.g += G * node.mass * dx / r ** 3
+            node0.g += gc * node.mass * dx / r ** 3
         else:
             # otherwise split up the node and repeat
             for c in node.children:
-                TreeWalk(c, node0, thetamax, G)
+                tree_walk(c, node0, thetamax, gc)
 
 
-def GravAccel(points, masses, thetamax=0.7, G=grav_const):
+def grav_accel(points, masses, thetamax=.5, gc=grav_const):
     center = (np.max(points, axis=0) + np.min(points, axis=0)) / 2  # center of particle cluster
     topsize = np.max(np.max(points, axis=0) - np.min(points, axis=0))  # size of total bounding box
+
     leaves = []  # want to keep track of leaf nodes
     topnode = OctNode(center, topsize, masses, points, np.arange(len(masses)), leaves)  # build the tree
 
     accel = np.empty_like(points)
     for i, leaf in enumerate(leaves):
-        TreeWalk(topnode, leaf, thetamax, G)  # do field summation
+        tree_walk(topnode, leaf, thetamax, gc)  # do field summation
         accel[leaf.id] = leaf.g  # get the stored acceleration
 
     return accel
 
 
-def Gen_RandPrtcls(n_particles, n_iterations):
-    #ToDo implement proper mass and maybe collision
-    global m
-    x = np.random.normal(size=(n_particles, 3))
-    m = np.repeat(np.random.normal()*10, n_particles)
-
+def gen_rand_prtcls(n_particles, n_iterations):
+    global masses
+    positions = np.random.normal(size=(n_particles-1, 3))
+    masses = abs(np.random.normal(size=(n_particles-1, 1)))
+    # add gravitational center
+    positions = np.vstack((positions, (np.max(positions, axis=0) + np.min(positions, axis=0)) / 2))
+    masses = np.append(masses, max(masses)*5)
+    positions = [positions]
     # Computing trajectory
-    data = [x]
     for iteration in range(n_iterations):
-        data.append(data[-1] + GravAccel(data[-1], m))
-    return data
+        positions.append(positions[-1] + grav_accel(positions[-1], masses))
+    return positions
 
 
-def main():
-    while True:
-        n_frames = 1000
-        n_prtcls = 80
+def main(n_prtcls=50, n_frames=15000, scale=200):
 
-        # data = Gen_RandPrtcls(n_particles=5, n_iterations=100)
-        data = Gen_RandPrtcls(n_particles=n_prtcls, n_iterations=n_frames-1)
-        data = np.array(data)  # (n_iterations, n_particles, 3)
+    data = gen_rand_prtcls(n_particles=n_prtcls, n_iterations=n_frames-1)
+    data = np.array(data)
 
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
 
-        fig = plt.figure()
-        ax = p3.Axes3D(fig)
+    # Plot the first position for all particles
+    h_particles = ax.plot(*data[0].T, marker='.', ls='', markersize=3)[0]
 
-        # Plot the first position for all particles
-        h = ax.plot(*data[0].T, marker='.', linestyle='None', markersize=3, color="y")[0]
+    # Setting the axes properties
+    ax.set_xlim3d([-1*scale, 1*scale])
 
-        # Setting the axes properties
+    ax.set_ylim3d([-1*scale, 1*scale])
 
-        #ax.set_xlim3d([min(data[-1][:,0])*1.1, max(data[-1][:,0])*1.1])
-        ax.set_xlim3d([-125, 125])
-        ax.set_xlabel('X')
+    ax.set_zlim3d([-1*scale, 1*scale])
+    ax.set_title(f'Particle sim (n={n_prtcls})', color='white')
 
-        #ax.set_ylim3d([min(data[-1][:,1])*1.1, max(data[-1][:,1])*1.1])
-        ax.set_ylim3d([-125, 125])
-        ax.set_ylabel('Y')
+    ax.set_axis_off()
+    ax.set_facecolor('black')
 
-        #ax.set_zlim3d([min(data[-1][:,2])*1.1, max(data[-1][:,1])*1.1])
-        ax.set_zlim3d([-125, 125])
-        ax.set_zlabel('Z')
-        ax.set_title(f'Particle sim (n={n_prtcls})', color='white')
+    def update_particles(num):
+        # Plot the iterations up to num for all particles
+        h_particles.set_xdata(data[num, :, 0].ravel())
+        h_particles.set_ydata(data[num, :, 1].ravel())
+        h_particles.set_3d_properties(data[num, :, 2].ravel())
+        return h_particles
 
-        ax.set_axis_off()
-        ax.set_facecolor('black')
+    prtcl_ani = animation.FuncAnimation(fig, update_particles, frames=n_frames, interval=1)
 
-        def update_particles(num):
-            # Plot the iterations up to num for all particles
-            h.set_xdata(data[num, :, 0].ravel())
-            h.set_ydata(data[num, :, 1].ravel())
-            h.set_3d_properties(data[num, :, 2].ravel())
-            return h
-
-        prtcl_ani = animation.FuncAnimation(fig, update_particles, frames=n_frames,
-                                            interval=33)
-
-        plt.show()
-
-        if 'q' in input("Enter q to quit: "):
-            break
-
-    return
+    plt.show()
 
 
 if __name__ == '__main__':
